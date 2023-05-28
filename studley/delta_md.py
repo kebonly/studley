fef6cea4c755ea2e79bf7b59c936a2afc21e3d1f
kebonly <kevin.sly99@gmail.com>
2023-05-28 19:55:01 -0400

    Added Obsidian daily activity logger

---
+++ studley/delta_md.py
@@ -0,0 +1,46 @@
+"""Module that summarizes all the changes in a directory"""
+
+import os
+import datetime
+import time
+import pickle
+
+PATH_OBSIDIAN = os.path.expanduser("~") + "/Ushnisha"
+PATH_OUTPUT = "activity_report.md"
+OMIT = {
+    ".DS_Store"
+}
+
+def save_modified(path_input, modified) -> None:
+    with open(path_input, "wb") as f:
+        pickle.dump(modified, f)
+
+def load_modified(path_input) -> set:
+    if os.path.isfile(path_input):
+        with open(path_input, "rb") as f:
+            data = pickle.load(f)
+        return data
+    else:
+        return set()
+
+
+if __name__ == "__main__":
+    today = datetime.datetime.today()
+    daystart = datetime.datetime(year=today.year, month=today.month, day=today.day, hour=0, second=0)
+    path_pickle = f"{os.path.expanduser('~')}/Projects/studley/pickles/{daystart.date()}.pickle"
+    prev_res = load_modified(path_pickle)# res = set()
+    res = set()
+    for filename in os.listdir(PATH_OBSIDIAN):
+        modify_time = datetime.datetime.fromtimestamp(os.path.getmtime(f"{PATH_OBSIDIAN}/{filename}"))
+        if (modify_time >= daystart) and os.path.isfile(f"{PATH_OBSIDIAN}/{filename}") and (filename not in OMIT):
+            if filename[-3:] == ".md":
+                res.add(filename[:-3])
+            else:
+                res.add(filename)
+
+    save_modified(path_pickle, res)
+    markdown_output = "" #f"\n---\n### Activity report for today\n"
+    for i in res - prev_res:
+        markdown_output += f"- [[{i}]]\n"
+    with open(f"{PATH_OBSIDIAN}/Daily notes/{daystart.date()}.md", "a") as f:
+        f.write(markdown_output)
