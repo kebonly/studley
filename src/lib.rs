4df74f7b9f24f410f9944fd7411d28c9794b4add
kebonly <kevin.sly99@gmail.com>
2023-05-28 17:29:36 -0400

    Able to plot results from Rust to Python

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
 use pyo3::prelude::*;
+pub mod diffusion;
+use rand::Rng;
 
 /// Formats the sum of two numbers as string.
 #[pyfunction]
@@ -6,9 +8,25 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
     Ok((a + b).to_string())
 }
 
+/// Returns an array of the first 100 time steps of 1-dimensional diffusion
+#[pyfunction]
+fn simulate_diffusion(diffusivity_constant: f64, x_0: f64, t: usize) -> PyResult<Vec<f64>> {
+
+    let mut res: Vec<f64> = vec![0.; t];
+    let mut rng = rand::thread_rng();
+    let mut current: f64 = x_0;
+
+    for x in res.iter_mut() {
+        *x = current;
+        current += rng.gen_range(-diffusivity_constant..diffusivity_constant);
+    }
+    Ok(res)
+}
+
 /// A Python module implemented in Rust.
 #[pymodule]
 fn string_sum(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(simulate_diffusion, m)?)?;
     Ok(())
 }
--- studley/main.py
+++ studley/main.py
@@ -1,2 +1,12 @@
 import string_sum
-print(type(string_sum.sum_as_string(3, 5)))
+import matplotlib.pyplot as plt
+
+# print(string_sum.sum_as_string(3, 5))
+# print(string_sum.simulate_diffusion(1.0, 3.5, 100))
+
+
+# print(type(res))
+for i in range(5):
+    res = string_sum.simulate_diffusion(1.0, 0, 1000)
+    plt.plot(res)
+plt.show()
