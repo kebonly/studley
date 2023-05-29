81c92ba02f581f876ea7e508042a392accc40999
kebonly <kevin.sly99@gmail.com>
2023-05-28 23:30:58 -0400

    Updates when literature notes are added and modified

--- studley/delta_md.py
+++ studley/delta_md.py
@@ -8,7 +8,12 @@ import pickle
 PATH_OBSIDIAN = os.path.expanduser("~") + "/Ushnisha"
 PATH_OUTPUT = "activity_report.md"
 OMIT = {
-    ".DS_Store"
+    ".DS_Store",
+    "Zotero Library Export.bib"
+}
+PATHS = {
+    os.path.expanduser("~") + "/Ushnisha",
+    os.path.expanduser("~") + "/Ushnisha/Literature Notes"
 }
 
 def save_modified(path_input, modified) -> None:
@@ -23,20 +28,20 @@ def load_modified(path_input) -> set:
     else:
         return set()
 
-
 if __name__ == "__main__":
     today = datetime.datetime.today()
     daystart = datetime.datetime(year=today.year, month=today.month, day=today.day, hour=0, second=0)
     path_pickle = f"{os.path.expanduser('~')}/Projects/studley/pickles/{daystart.date()}.pickle"
     prev_res = load_modified(path_pickle)# res = set()
     res = set()
-    for filename in os.listdir(PATH_OBSIDIAN):
-        modify_time = datetime.datetime.fromtimestamp(os.path.getmtime(f"{PATH_OBSIDIAN}/{filename}"))
-        if (modify_time >= daystart) and os.path.isfile(f"{PATH_OBSIDIAN}/{filename}") and (filename not in OMIT):
-            if filename[-3:] == ".md":
-                res.add(filename[:-3])
-            else:
-                res.add(filename)
+    for dirs in PATHS:
+        for filename in os.listdir(dirs):
+            modify_time = datetime.datetime.fromtimestamp(os.path.getmtime(f"{dirs}/{filename}"))
+            if (modify_time >= daystart) and os.path.isfile(f"{dirs}/{filename}") and (filename not in OMIT):
+                if filename[-3:] == ".md":
+                    res.add(filename[:-3])
+                else:
+                    res.add(filename)
 
     save_modified(path_pickle, res)
     markdown_output = "" #f"\n---\n### Activity report for today\n"
